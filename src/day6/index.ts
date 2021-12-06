502ab7159c97fd027361d52ee762f0a7e6670064
Tomer Peleg <tomer.peleg@sky.uk>
2021-12-06 15:53:22 +0000

    Day 6 p2

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -2,11 +2,6 @@ import getInput from "../util/getInput";
 import path from "path";
 import util from "util";
 
-type Fish = {
-  timer: number;
-  age: number;
-};
-
 const FISH_TIME = 6;
 const NEW_FISH_TIME = 8;
 
@@ -32,6 +27,34 @@ const part1 = (fishes: Array<number>, days: number) => {
 
   return _fishes.length;
 };
+const part2 = (fishes: Array<number>, days: number) => {
+  let fishTimers = new Array(NEW_FISH_TIME + 1).fill(0);
+
+  for (let fish of fishes) {
+    fishTimers[fish]++;
+  }
+
+  let totalFish = fishes.length;
+  // console.log(
+  //   "Initial state: \t",
+  //   fishTimers.map((n, i) => `${i}: ${n}`).join(", ")
+  // );
+
+  for (let i = 0; i < days; i++) {
+    const newFish = fishTimers.shift();
+    fishTimers[NEW_FISH_TIME] = newFish;
+    fishTimers[FISH_TIME] += newFish;
+    totalFish += newFish;
+    // console.log(
+    //   "After day",
+    //   i,
+    //   "\t",
+    //   fishTimers.map((n, i) => `${i}: ${n}`).join(", ")
+    // );
+  }
+
+  return totalFish;
+};
 
 export default async () => {
   const data: string = await getInput(path.join(__dirname, "./input"));
@@ -41,7 +64,13 @@ export default async () => {
 
   console.log("P1 ----------");
   console.time("p1");
-  const p1Result = part1(fishes, 80);
+  const p1Result = part1(fishes, 18);
   console.timeEnd("p1");
   console.log("P1 Result: ", p1Result);
+
+  console.log("P2 ----------");
+  console.time("p2");
+  const p2Result = part2(fishes, 256);
+  console.timeEnd("p2");
+  console.log("P2 Result: ", p2Result);
 };
