f6976c894122a3eb8a3c676a8009b4845ca214e8
Tomer Peleg <tomer.peleg@gmail.com>
2021-12-18 14:47:17 +0000

    Day 14 p2

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -5,6 +5,7 @@ import util from "util";
 type Insert = {
   left: string;
   right: string;
+  pair?: string;
   insert: string;
 };
 
@@ -14,6 +15,7 @@ const dim = (str: string) => `\x1b[2m${str}\x1b[0m`;
 const parseInsertion = (input: string): Insert => ({
   left: input[0],
   right: input[1],
+  pair: input[0] + input[1],
   insert: input[6],
 });
 
@@ -73,6 +75,68 @@ const part1 = (template: string, insertions: Array<Insert>, repeat = 1) => {
   return sortedCounts[sortedCounts.length - 1] - sortedCounts[0];
 };
 
+const getPairs = (template: string) => {
+  let pairs: Record<string, number> = {};
+  let chars: Record<string, number> = {};
+
+  for (let i = 0; i < template.length - 1; i++) {
+    chars[template[i]] = (chars[template[i]] || 0) + 1;
+    pairs[template[i] + template[i + 1]] =
+      (pairs[template[i] + template[i + 1]] || 0) + 1;
+  }
+  chars[template[template.length - 1]] =
+    (chars[template[template.length - 1]] || 0) + 1;
+
+  return { pairs, chars };
+};
+
+const doPairInserts = (
+  pairs: Record<string, number>,
+  chars: Record<string, number>,
+  insertions: Array<Insert>
+) => {
+  let newPairs: Record<string, number> = {};
+  let newChars: Record<string, number> = { ...chars };
+
+  for (let insert of insertions) {
+    const pair = insert.left + insert.right;
+    if (pairs.hasOwnProperty(pair) && pairs[pair] > 0) {
+      const prevPair = insert.left + insert.right;
+      const newPairLeft = insert.left + insert.insert;
+      const newPairRight = insert.insert + insert.right;
+
+      newPairs[newPairLeft] = (newPairs[newPairLeft] || 0) + pairs[prevPair];
+      newPairs[newPairRight] = (newPairs[newPairRight] || 0) + pairs[prevPair];
+      newChars[insert.insert] =
+        (newChars[insert.insert] || 0) + pairs[prevPair];
+    }
+  }
+
+  return { pairs: newPairs, chars: newChars };
+};
+
+const part2 = (template: string, insertions: Array<Insert>, repeat = 1) => {
+  let { pairs, chars } = getPairs(template);
+  // console.log(
+  //   "Start:",
+  //   bright(template),
+  //   "doing insertions:",
+  //   insertions.length,
+  //   { pairs, chars }
+  // );
+  for (let i = 0; i < repeat; i++) {
+    const result = doPairInserts(pairs, chars, insertions);
+    pairs = result.pairs;
+    chars = result.chars;
+  }
+
+  const sortedCounts = Object.values(chars)
+    .filter((n) => n > 0)
+    .sort((a, b) => (a > b ? 1 : -1));
+
+  return sortedCounts[sortedCounts.length - 1] - sortedCounts[0];
+};
+
 export default async () => {
   const data: string = await getInput(path.join(__dirname, "./input"));
   const input = parseInput(data);
@@ -82,6 +146,14 @@ export default async () => {
   console.log("P1 ----------");
   console.time("p1");
   const p1Result = part1(input.template, input.insertions, 10);
+
   console.timeEnd("p1");
   console.log("P1 Result: ", p1Result);
+
+  console.log("p2 ----------");
+  console.time("p2");
+  const p2Result = part2(input.template, input.insertions, 40);
+
+  console.timeEnd("p2");
+  console.log("p2 Result: ", p2Result);
 };
